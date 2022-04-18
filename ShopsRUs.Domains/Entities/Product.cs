e7de5d9af80d5840af17fb1ed16120fc1acb3e96
emrahsinekli <emrahsinekli@gmail.com>
2022-04-18 16:16:16 +0300

    Added UML Class Diagram

--- ShopsRUs.Domains/Entities/Product.cs
+++ ShopsRUs.Domains/Entities/Product.cs
@@ -12,7 +12,6 @@ namespace ShopsRUs.Domains.Entities
     {
    
         public int Id { get; set; }
-
         public string ProductName { get; set; }
         public decimal ProductPrice { get; set; }
         public decimal DerivedProductCost { get; set; }
